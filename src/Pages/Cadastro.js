f11866c176fcbdd8e983f30ec8f5f0ff6caf7a6a
anderson-nunes <andersonribeironunes@hotmail.com>
2023-08-04 10:49:34 -0300

    Exercicio conteúdo estado global react

--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -1,66 +1,83 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { handleHome } from "../Router/cordinator";
 import styled from "styled-components";
 
-export default function Cadastro (props){
-  const[formulario, setFormulario]=useState({name:"", url:"", price:""})
+export default function Cadastro({ frutas, setFrutas }) {
+  const [formulario, setFormulario] = useState({
+    name: "",
+    url: "",
+    price: ""
+  })
+
+  console.log('aquii==>>', formulario)
+
   const navigate = useNavigate();
 
-  const onChangeInputs=(event)=>{
-    const {name, value}= event.target
-    setFormulario({...formulario, [name]:value})
+  const onChangeInputs = (event) => {
+    const { name, value } = event.target
+    setFormulario({ ...formulario, [name]: value })
   }
-  const handleClick = (event)=>{
+
+
+  const handleClick = (event) => {
     event.preventDefault()
+
     console.log(formulario);
+
+    const newFruta = { ...formulario, id: Date.now() }
+
+    setFrutas([...frutas, newFruta])
   }
- 
- 
-  return(
+
+
+  return (
     <CadastroContainer>
       <h1>Cadastro</h1>
       <button onClick={() => handleHome(navigate)}>Voltar</button>
-    <FormContainer onSubmit={handleClick}>
-      <Input
-      name="name"
-      value={formulario.name}
-      onChange={onChangeInputs}
-      type="text"
-      placeholder="Nome da Fruta"
-      />
-      <Input 
-      name="url"
-      value={formulario.url}
-      onChange={onChangeInputs}
-      type="text"
-      placeholder="url da imagem"
-      />
-      <Input
-      name="price"
-      value={formulario.price}
-      onChange={onChangeInputs}
-      type="number"
-      placeholder="Preço da Fruta"
-      />
-      <button> Cadastrar </button>
-    </FormContainer>
+      <FormContainer onSubmit={handleClick}>
+        <Input
+          name="name"
+          value={formulario.name}
+          onChange={onChangeInputs}
+          type="text"
+          placeholder="Nome da Fruta"
+        />
+        <Input
+          name="url"
+          value={formulario.url}
+          onChange={onChangeInputs}
+          type="text"
+          placeholder="url da imagem"
+        />
+        <Input
+          name="price"
+          value={formulario.price}
+          onChange={onChangeInputs}
+          type="number"
+          placeholder="Preço da Fruta"
+        />
+        <button> Cadastrar </button>
+      </FormContainer>
     </CadastroContainer>
   )
 }
+
+
 const CadastroContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
+
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
+
 const Input = styled.input`
 padding: 10px;
 margin: 8px;
-width:200px;
- 
+width:200px; 
 `;
--- src/Pages/Mercadinho.js
+++ src/Pages/Mercadinho.js
@@ -1,39 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { handleCart} from "../Router/cordinator";
+import { handleCart, handleCadastro } from "../Router/cordinator";
 import CardFrutas from "../components/CardFrutas";
-import frutaria from "../frutaria.json";
 import styled from "styled-components";
 
-export default function Mercadinho(props) {
-    const [frutas, setFrutas] = useState(frutaria.frutaria);
+export default function Mercadinho({ carrinho, setCarrinho, frutas }) {
     const navigate = useNavigate();
 
     function comprar(id) {
-        // const i = carrinho.findIndex((item) => item.id === id);
-        // console.log(i);
-        // if (i !== -1) {
-        //   carrinho[i] = { ...carrinho[i], amount: carrinho[i].amount + 1 };
-        // } else {
-        //   const frutaEncontrada = frutas.find((fruta) => fruta.id === id);
-        //   const novaFruta = { ...frutaEncontrada, amount: 1 };
-        //   const novaLista = [...carrinho, (carrinho[1] = novaFruta)];
-
-        //   setCarrinho(novaLista);
+        const i = carrinho.findIndex((item) => item.id === id);
+        if (i !== -1) {
+            carrinho[i] = { ...carrinho[i], amount: carrinho[i].amount + 1 };
+        } else {
+            const frutaEncontrada = frutas.find((fruta) => fruta.id === id);
+            const novaFruta = { ...frutaEncontrada, amount: 1 };
+            const novaLista = [...carrinho, (carrinho[1] = novaFruta)];
+            setCarrinho(novaLista);
+        }
     }
 
-
-
-return (
-    <MercadinhoContainer>
-        <h1>Mercadinho</h1>
-        <button onClick={() => handleCart(navigate)}>Vá para Carrinho </button>
-        <button>Cadastro de Frutas </button>
-        <FrutasContainer>
-            <CardFrutas />
-        </FrutasContainer>
-    </MercadinhoContainer>
-);
+    return (
+        <MercadinhoContainer>
+            <h1>Mercadinho</h1>
+            <button onClick={() => handleCart(navigate)}>Vá para Carrinho </button>
+            <button onClick={() => handleCadastro(navigate)}>
+                Cadastro de Frutas{" "}
+            </button>
+            <FrutasContainer>
+                {frutas.map((fruta) => {
+                    return <CardFrutas key={fruta.id} fruta={fruta} comprar={comprar} />;
+                })}
+            </FrutasContainer>
+        </MercadinhoContainer>
+    );
 }
 const FrutasContainer = styled.section`
   display: flex;
